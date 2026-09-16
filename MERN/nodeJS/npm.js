89ebf81ffa19527f0f750f7d18e738d0bdf54dc0
Sandhu005 <love.sandhu.mail@gmail.com>
2026-09-16 14:43:09 +0530

    laravel project

--- MERN/nodeJS/npm.js
+++
@@ -1,5 +0,0 @@
-//Generating random name using NPM package
-var generateName = require('sillyname');
-var sillyName = generateName();
-
-console.log(`My Name is ${sillyName}.`);
--- MERN/nodeJS/readFile.js
+++
@@ -1,8 +0,0 @@
-
-// Reading file using node modules
-const fs = require("fs");
-
-fs.readFile("msg.txt", "utf8", (err, data)=> {
-    if (err) throw err;
-    console.log(data);
-});
